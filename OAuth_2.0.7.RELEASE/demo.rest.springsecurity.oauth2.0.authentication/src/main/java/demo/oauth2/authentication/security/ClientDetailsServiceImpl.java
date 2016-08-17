9f856caa8be51bdb2cf225dfdc8ebbb3646dd40c
pa <prateek.ashtikar512@yahoo.in>
2016-08-17 11:14:09 +0530

    Spring OAuth2, Spring Security OAuth2 v 2.0.10.RELEASE

--- OAuth_2.0.7.RELEASE/demo.rest.springsecurity.oauth2.0.authentication/src/main/java/demo/oauth2/authentication/security/ClientDetailsServiceImpl.java
+++ OAuth_2.0.7.RELEASE/demo.rest.springsecurity.oauth2.0.authentication/src/main/java/demo/oauth2/authentication/security/ClientDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package demo.oauth2.authentication.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -12,27 +14,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ClientDetailsServiceImpl implements ClientDetailsService {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClientDetailsServiceImpl.class);
+	
 	private static final String CLIENT_CREDENTIALS = "client_credentials";
 	private static final String REFRESH_TOKEN = "refresh_token";
 	private static final String PASSWORD = "password";
 
 	@Override
 	public ClientDetails loadClientByClientId(String clientId) throws OAuth2Exception {
+		LOGGER.debug("~~~~ Executing loadClientByClientId() of ClientDetailsServiceImpl ~~~~");
+		
 		List<String> authorizedGrantTypes = new ArrayList<>();
 		authorizedGrantTypes.add(PASSWORD);
 		authorizedGrantTypes.add(REFRESH_TOKEN);
 		authorizedGrantTypes.add(CLIENT_CREDENTIALS);
 
+		BaseClientDetails clientDetails = new BaseClientDetails();
 		if (clientId.equals("client1")) {
-			BaseClientDetails clientDetails = new BaseClientDetails();
 			clientDetails.setClientId("client1");
 			clientDetails.setClientSecret("client1");
 			clientDetails.setAuthorizedGrantTypes(authorizedGrantTypes);
 			return clientDetails;
 		} 
 		else if(clientId.equals("client2")){
-			BaseClientDetails clientDetails = new BaseClientDetails();
 			clientDetails.setClientId("client2");
 			clientDetails.setClientSecret("client2");
 			clientDetails.setAuthorizedGrantTypes(authorizedGrantTypes);
